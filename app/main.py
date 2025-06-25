8e45d68ec6e1a52faf561f3fe875fdb7d10a307c
Ravi Rajput <ravi.rajput@contractors.roche.com>
2025-06-25 19:41:06 +0530

    releasing version 1.0

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask app v1.1 deployed to GKE via Cloud Build CI/CD!"
+    return "Hello from Flask app v1.0 deployed to GKE via Cloud Build CI/CD!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
