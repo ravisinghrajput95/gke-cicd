4043c27652967a5bd6ceb695c9accebca1eb0d56
Ravi Rajput <ravi.rajput@contractors.roche.com>
2025-06-26 23:41:10 +0530

    update

--- terraform/auto-pilot/data.tf
+++ terraform/auto-pilot/data.tf
@@ -1,6 +1,1 @@
 data "google_client_config" "default" {}
-
-data "google_container_engine_versions" "region" {
-  location = var.region
-  project  = var.project_id
-}
--- terraform/standard/data.tf
+++ terraform/standard/data.tf
@@ -1,11 +1,1 @@
-data "google_container_engine_versions" "zone" {
-  project  = "silent-presence-463914-s2"
-  location = "us-central1-a"
-}
-
-data "google_container_engine_versions" "region" {
-  project  = "silent-presence-463914-s2"
-  location = "us-central1"
-}
-
 data "google_client_config" "default" {}
